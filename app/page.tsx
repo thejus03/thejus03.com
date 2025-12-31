4ca6388a4583d3f0bc77271dd3332b362dafbf78
Thejus <thejusunni03@gmail.com>
2025-12-31 13:21:04 +0800

    Refactor homepage layout and remove unused components for improved performance

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div
-      className="flex min-h-screen px-5 flex-col max-w-3xl mx-auto text-white mb-20"
+      className="flex min-h-screen px-5 flex-col max-w-3xl mx-auto text-white my-12"
     >
-      <Navbar />
+      {/* <Navbar /> */}
       <div>
         <div className="text-[16px]">
-          <h1 className="font-bold text-2xl py-4">{'> '}whoami</h1>
+          <h1 className="font-bold text-zinc-300 text-2xl py-2 px-4 rounded-xl border border-stone-700 bg-stone-800 inline-block">{'> '}whoami</h1>
           <p className="py-3">Hi, I'm Thejus 👾</p>
           <p className="py-3">
             I'm a software engineer based in Singapore. I am currently a
@@ -21,7 +20,7 @@ export default function Home() {
           <p className="py-3">
             Feel free to contact me via <a className="hover:underline text-rose-500" href="mailto:thejusunni@hotmail.com" target="_blank" rel="noopener noreferrer">email</a> or <a className="hover:underline text-rose-500" href="https://t.me/thejus03" target="_blank" rel="noopener noreferrer">telegram</a>.
           </p>
-          <h1 className="font-bold text-2xl py-4">{'> '}date</h1>
+          <h1 className="font-bold text-zinc-300 text-2xl py-2 px-4 rounded-xl border border-stone-700 bg-stone-800 inline-block mt-10">{'> '}date</h1>
           <p className="py-3">
             I'm <b>currently</b>:
           </p>
@@ -30,14 +29,14 @@ export default function Home() {
             <li>Building side projects or working on open-source software</li>
             <li>Trying to catch up on my Netflix watchlist</li>
           </ul>
-          <h1 className="font-bold text-2xl py-4">{'> '}grep "work"</h1>
+          <h1 className="font-bold text-zinc-300 text-2xl py-2 px-4 rounded-xl border border-stone-700 bg-stone-800 inline-block mt-10">{'> '}grep "work" *</h1>
           <p className="py-3">
             Previously, I've <b>interned</b> as a:
           </p>
           <ul className="list-disc ml-6 pb-3">
             <li>Software Engineer at <a className="hover:underline text-rose-500" href="https://www.invigilo.ai" target="_blank" rel="noopener noreferrer">Invigilo AI</a></li>
           </ul>
-          <h1 className="font-bold text-2xl py-4">{'> '}grep "projects"</h1>
+          <h1 className="font-bold text-zinc-300 text-2xl py-2 px-4 rounded-xl border border-stone-700 bg-stone-800 inline-block mt-10">{'> '}grep "projects" *</h1>
           <p className="my-3">
             Few fun <b>projects</b> I've worked on:
           </p>
@@ -46,6 +45,7 @@ export default function Home() {
             <li><a href="https://tracktern.com" className="hover:underline text-rose-500" target="_blank" rel="noopener noreferrer">TrackTern</a> - platform for tracking internships (used by 600+ users)</li>
             <li><a href="https://github.com/thejus03/portfoliomatic" className="hover:underline text-rose-500" target="_blank" rel="noopener noreferrer">Financial portfolio robo-advisory tool</a> - built for fun</li>
             <li><a href="https://github.com/thejus03/chessai" className="hover:underline text-rose-500" target="_blank" rel="noopener noreferrer">AI Chess engine</a> - thinks 5 moves ahead in less than 10 seconds</li>
+            <li><a href="https://github.com/thejus03/Text-Emotion-Neural" className="hover:underline text-rose-500" target="_blank" rel="noopener noreferrer">Text Emotion Recognition</a> - first and only neural network i created</li>
           </ul>
           
         </div>
--- components/Navbar.tsx
+++
@@ -1,8 +0,0 @@
-export default function Navbar() {
-    return (
-        <nav className="w-full p-10 bg-transparent flex justify-center font-semibold text-lg">
-            <div className="flex space-x-8 text-2xl">
-            </div>
-        </nav>
-    )
-}
